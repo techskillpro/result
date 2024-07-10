f82e6d95a04acbc999f9b0b88027a1f3fe902cbf
techskillpro <olutechitconsult@gmail.com>
2024-07-10 16:24:28 +0100

    second commit

--- Result/settings.py
+++ Result/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = 'static/'
 STATIC_DIR = [
     BASE_DIR / 'static'
 ]
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
